9506218545c64c27e8a503944bb7a11b2e0add43
Remya Nekkuth Melath <remyaanoop0731@gmail.com>
2026-07-27 12:04:55 -0700

    Updated terraform file

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformbuckettoday" {
+  bucket = "terraformbuckettoday-2026-eu-west-2"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformbuckettoday-2026-eu-west-2"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
